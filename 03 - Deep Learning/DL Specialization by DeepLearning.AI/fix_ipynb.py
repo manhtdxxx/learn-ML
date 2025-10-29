b89fa6877231d0c90e6b3f74964cb59e3c1bb42a
Tran Duc Manh <tdmanh1510@gmail.com>
2025-10-29 20:59:59 +0700

    .

--- 03 - Deep Learning/DL Specialization by DeepLearning.AI/fix_ipynb.py
+++ 03 - Deep Learning/DL Specialization by DeepLearning.AI/fix_ipynb.py
@@ -1,17 +1,19 @@
 import nbformat
 
-# Path to the notebook you want to fix
-notebook_path = "C5_W4_N3 - Question Answering.ipynb"
 
-# Read the notebook
-nb = nbformat.read(notebook_path, as_version=4)
+NOTEBOOKS = ["C5_W4_N3 - Question Answering.ipynb"]  # List of notebooks or use glob
+WIDGETS_OPTION = "remove"  # "fix" or "remove"
 
-# Fix all cells that have metadata.widgets
-for cell in nb.cells:
-    if "metadata" in cell and "widgets" in cell.metadata:
-        cell.metadata["widgets"]["state"] = {}
 
-# Write the notebook back
-nbformat.write(nb, notebook_path)
+for notebook_path in NOTEBOOKS:
+    nb = nbformat.read(notebook_path, as_version=4)
 
-print(f"Fixed metadata.widgets in {notebook_path}")
+    for cell in nb.cells:
+        if "metadata" in cell and "widgets" in cell.metadata:
+            if WIDGETS_OPTION == "fix":
+                cell.metadata["widgets"]["state"] = {}
+            elif WIDGETS_OPTION == "remove":
+                del cell.metadata["widgets"]
+
+    nbformat.write(nb, notebook_path)
+    print(f"Fixed widgets in notebook: {notebook_path}, Widgets option: {WIDGETS_OPTION}")
